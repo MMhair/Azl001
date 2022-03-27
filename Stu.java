1657052e45d149efad24bc866d31b9050f9ca957
MMhair <102365046+MMhair@users.noreply.github.com>
2022-03-27 20:26:46 +0800

    Add files via upload

---
+++ Stu.java
@@ -0,0 +1,16 @@
+package oop;
+
+/**
+ * 学生类
+ */
+public class Stu {
+    int id;
+    int age;
+    String name;
+    public void study() {
+        System.out.println("学习");
+    }
+    public void PlayFootball() {
+        System.out.println("踢足球");
+    }
+}
